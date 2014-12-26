18e0a3409690b4971d3af04d806112c109e8b122
Joel Small <joel.small@biscon.com.au>
2014-12-26 19:56:47 +1030

    initial commit

---
+++ Parameters.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joel Small
+ * Date: 26/12/14
+ * Time: 2:17 PM
+ */
+
+namespace common\helpers;
+
+class Parameters {
+
+    static function jsonify($array)
+    {
+        $string = '';
+        foreach ($array as $key => $value)
+        {
+            $string .= $key .': ' . self::jsonifyValue($value) .',
+            ';
+        }
+        return "{".$string."}";
+    }
+
+    static function jsonifyValue($value)
+    {
+        if(is_array($value))
+        {
+            return self::jsonify($value);
+        }
+        if(strpos($value,'function') === false)
+        {
+            $value = '"'.$value.'"';
+        }
+        return $value;
+    }
+
+} 
